8a9d10031389d495e57e1a752a44758fe9a8e6a3
Daniel Lewis <daniellewis777@gmail.com>
2024-01-02 01:10:41 +0000

    language

--- src/app/components/chord-highlighter/data/chords-and-lyrics-language.ts
+++ src/app/components/chord-highlighter/data/chords-and-lyrics-language.ts
@@ -2,11 +2,41 @@ import { LanguageSupport, StreamLanguage, StringStream } from '@codemirror/langu
 import { tags as t } from '@lezer/highlight'
 import { completeFromList } from '@codemirror/autocomplete'
 import { Extension } from '@codemirror/state'
+import { cloneDeep } from 'lodash-es'
 
-export const ChordsAndLyricsLanguage: StreamLanguage<any> = StreamLanguage.define({
+interface IState {
+  db: boolean
+  collection: boolean
+  where: boolean
+  limit: boolean
+  whereValue: boolean
+  whereOperator: boolean
+}
+
+const initialState: IState = {
+  db: false,
+  collection: false,
+  where: false,
+  limit: false,
+  whereValue: false,
+  whereOperator: false
+}
+
+// https://stackoverflow.com/questions/75191956/how-to-properly-define-a-codemirror-language
+// https://codemirror.net/docs/ref/#language.StreamParser
+
+export const ChordsAndLyricsLanguage: StreamLanguage<IState> = StreamLanguage.define({
+  // A name for this language
   name: 'ChordsAndLyrics',
-  startState: (/* indentUnit: number */) => ({}),
-  token: (stream: StringStream, state: any = {}): string => {
+
+  // Produce a start state for the parser
+  startState: (/* indentUnit: number */): IState => initialState,
+
+  // Read one token, advancing the stream past it, and returning a string indicating the token's style tag
+  // either the name of one of the tags in tags or tokenTable, or such a name suffixed by one or more tag modifier names, separated by periods
+  // For example "keyword" or "variableName.constant", or a space-separated set of such token types
+  // It is okay to return a zero-length token, but only if that updates the state so that the next call will return a non-empty token again
+  token: (stream: StringStream, state: IState = initialState): string => {
     if (stream.match('db')) {
       state.db = true
       return 'keyword'
@@ -82,12 +112,23 @@ export const ChordsAndLyricsLanguage: StreamLanguage<any> = StreamLanguage.defin
     stream.next()
     return null
   },
-  blankLine: (/* state: {}, indentUnit: number */) => {},
-  copyState: (/* state: {} */) => {},
-  indent: (/* state: {}, textAfter: string, context: IndentContext */): number => 1,
+
+  // This notifies the parser of a blank line in the input. It can update its state here if it needs to
+  blankLine: (/* state: IState, indentUnit: number */) => {},
+
+  // Copy a given state. By default, a shallow object copy is done which also copies arrays held at the top level of the object
+  copyState: (state: IState): IState => cloneDeep(state),
+
+  // Compute automatic indentation for the line that starts with the given state and text
+  indent: (/* state: IState, textAfter: string, context: IndentContext */): number => 1,
+
+  // Default language data to attach to this language
   languageData: {
     commentTokens: { line: ';' }
   },
+
+  // Extra tokens to use in this parser. When the tokenizer returns a token name that exists as a property in this object,
+  // the corresponding tags will be assigned to the token
   tokenTable: {
     db: t.keyword,
     dot: t.punctuation,
@@ -98,6 +139,8 @@ export const ChordsAndLyricsLanguage: StreamLanguage<any> = StreamLanguage.defin
     string: t.string
   }
 })
+
+// https://codemirror.net/examples/autocompletion
 
 export const ChordsAndLyricsCompletion: Extension = ChordsAndLyricsLanguage.data.of({
   autocomplete: completeFromList([
