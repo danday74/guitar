25fa40fa5822494d61f867b1e46677ba62aa02fc
Daniel Lewis <daniellewis777@gmail.com>
2024-01-02 03:09:50 +0000

    theme

--- src/app/components/chord-highlighter/data/codemirror-extensions.ts
+++ src/app/components/chord-highlighter/data/codemirror-extensions.ts
@@ -7,6 +7,7 @@ import {
   highlightSpecialChars,
   keymap,
   lineNumbers,
+  placeholder,
   rectangularSelection
 } from '@codemirror/view'
 import { defaultKeymap, history, historyKeymap } from '@codemirror/commands'
@@ -15,36 +16,52 @@ import { EditorState, Extension } from '@codemirror/state'
 import { autocompletion, closeBrackets, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete'
 import { highlightSelectionMatches, searchKeymap } from '@codemirror/search'
 import { lintKeymap } from '@codemirror/lint'
+import { ICodemirrorExtensionOptions } from '@components/chord-highlighter/interfaces/i-codemirror-extension-options'
+import { birdsOfParadise, dracula } from 'thememirror'
 
 // see basicSetup from node_modules/codemirror/dist/index.js
 
-const codemirrorExtensions: Extension[] = (() => [
-  lineNumbers(),
-  highlightActiveLineGutter(),
-  highlightSpecialChars(),
-  history(),
-  foldGutter(),
-  drawSelection(),
-  dropCursor(),
-  EditorState.allowMultipleSelections.of(true),
-  indentOnInput(),
-  syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-  bracketMatching(),
-  closeBrackets(),
-  autocompletion(),
-  rectangularSelection(),
-  crosshairCursor(),
-  highlightActiveLine(),
-  highlightSelectionMatches(),
-  keymap.of([
-    ...closeBracketsKeymap,
-    ...defaultKeymap,
-    ...searchKeymap,
-    ...historyKeymap,
-    ...foldKeymap,
-    ...completionKeymap,
-    ...lintKeymap
-  ])
-])()
+const codemirrorExtensions = (options: ICodemirrorExtensionOptions): Extension[] => {
+  const extensions: Extension[] = [
+    lineNumbers(),
+    highlightActiveLineGutter(),
+    highlightSpecialChars(),
+    history(),
+    foldGutter(),
+    drawSelection(),
+    dropCursor(),
+    EditorState.allowMultipleSelections.of(true),
+    indentOnInput(),
+    syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+    bracketMatching(),
+    closeBrackets(),
+    autocompletion(),
+    rectangularSelection(),
+    crosshairCursor(),
+    highlightActiveLine(),
+    highlightSelectionMatches(),
+    keymap.of([
+      ...closeBracketsKeymap,
+      ...defaultKeymap,
+      ...searchKeymap,
+      ...historyKeymap,
+      ...foldKeymap,
+      ...completionKeymap,
+      ...lintKeymap
+    ]),
+    // additional extensions
+    placeholder(options.placeholder)
+  ]
+
+  // https://thememirror.net
+  switch (options.theme) {
+    case 'birdsOfParadise':
+      return [...extensions, birdsOfParadise]
+    case 'dracula':
+      return [...extensions, dracula]
+    default:
+      return extensions
+  }
+}
 
 export { codemirrorExtensions }
---
+++ src/app/components/chord-highlighter/interfaces/i-codemirror-extension-options.ts
@@ -0,0 +1,4 @@
+export interface ICodemirrorExtensionOptions {
+  placeholder: string
+  theme: 'birdsOfParadise' | 'dracula' | null
+}
