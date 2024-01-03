9e170cfa42c5fe0a4aafa63040d76da95aaf3621
Daniel Lewis <daniellewis777@gmail.com>
2024-01-03 23:37:12 +0000

    app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,20 @@
-import { ApplicationConfig } from '@angular/core'
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core'
 import { provideRouter, withComponentInputBinding } from '@angular/router'
 import { routes } from './app.routes'
+import { AppInitService } from '@services/app-init.service'
+
+const initializeAppFactory = (appInitService: AppInitService): () => Promise<boolean> => {
+  return () => appInitService.init()
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding())]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeAppFactory,
+      multi: true,
+      deps: [AppInitService]
+    }
+  ]
 }
