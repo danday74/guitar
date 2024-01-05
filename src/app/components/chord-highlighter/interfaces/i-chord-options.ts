43d9d610d07fe06b2bf047bd404f1bdd6fb8ca34
Daniel Lewis <daniellewis777@gmail.com>
2024-01-05 01:46:32 +0000

    chord options

---
+++ src/app/components/chord-highlighter/interfaces/i-chord-options.ts
@@ -0,0 +1,9 @@
+import { TSharpsFlats } from '@components/chord-highlighter/types/t-sharps-flats'
+
+export interface IChordOptions {
+  caseInsensitive: boolean
+  sharps: TSharpsFlats
+  flats: TSharpsFlats
+}
+
+export const defaultChordOptions: IChordOptions = Object.freeze({ caseInsensitive: true, sharps: true, flats: true })
