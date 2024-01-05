3c07e0bd3e566a054807daac0d95430fa14c7890
Daniel Lewis <daniellewis777@gmail.com>
2024-01-05 01:43:33 +0000

    chord utils

--- src/app/utils/chord-utils.ts
+++ src/app/utils/chord-utils.ts
@@ -1,5 +1,7 @@
-import { replaceAt } from './string-utils'
+import { onlyIncludes, replaceAt } from './string-utils'
 import { TChord } from '@components/chord-highlighter/types/t-chord'
+import { getChords } from '@components/chord-highlighter/data/chords'
+import { defaultChordOptions, IChordOptions } from '@components/chord-highlighter/interfaces/i-chord-options'
 
 export const toStandard = (chord: TChord): TChord => {
   if (chord.length > 1) {
@@ -7,4 +9,9 @@ export const toStandard = (chord: TChord): TChord => {
     if (chord[1] === 'b') chord = replaceAt(chord, 1, '♭') as TChord
   }
   return chord
+}
+
+export const isChordLine = (line: string, options: IChordOptions = defaultChordOptions): boolean => {
+  const chords: TChord[] = getChords(options)
+  return onlyIncludes(line, chords)
 }
