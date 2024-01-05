f161075926ba89fcc3107228e9ae793c737b29e1
Daniel Lewis <daniellewis777@gmail.com>
2024-01-05 02:54:21 +0000

    TODO

--- src/app/utils/string-utils.ts
+++ src/app/utils/string-utils.ts
@@ -1,7 +1,6 @@
 // https://stackoverflow.com/a/77744294/1205871
 export const onlyIncludes = (str: string, substrings: string[]): boolean => {
   // TODO: handle empty string?
-  // TODO: Ensure isChordLine is used instead wherever possible
   const words: string[] = str.trim().split(/\s+/)
   return words.every((word: string) => substrings.includes(word))
 }
