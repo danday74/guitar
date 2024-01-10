5fcfe160ba397c5b3932e7cc05eb574bde70af9e
Daniel Lewis <daniellewis777@gmail.com>
2024-01-10 03:09:42 +0000

    chord options

--- src/app/components/chord-highlighter/interfaces/i-chord-options.ts
+++ src/app/components/chord-highlighter/interfaces/i-chord-options.ts
@@ -1,5 +1,5 @@
-import { TSharpsFlats } from '@components/chord-highlighter/types/t-sharps-flats'
 import { TChordCase } from '@components/chord-highlighter/types/t-chord-case'
+import { TSharpsFlats } from '@components/chord-highlighter/types/t-sharps-flats'
 
 export interface IChordOptions {
   case: TChordCase
@@ -7,4 +7,10 @@ export interface IChordOptions {
   flats: TSharpsFlats
 }
 
-export const defaultChordOptions: IChordOptions = Object.freeze({ case: 'all', sharps: 'all', flats: 'all' })
+export const chordOptions: { [key in 'all' | 'standard' | 'lowerAll' | 'standardAll' | 'standardEasyType']: IChordOptions } = {
+  all: Object.freeze({ case: 'all', sharps: 'all', flats: 'all' }),
+  standard: Object.freeze({ case: 'standard', sharps: 'standard', flats: 'standard' }),
+  lowerAll: Object.freeze({ case: 'lower', sharps: 'all', flats: 'all' }),
+  standardAll: Object.freeze({ case: 'standard', sharps: 'all', flats: 'all' }),
+  standardEasyType: Object.freeze({ case: 'standard', sharps: 'easyType', flats: 'easyType' })
+}
