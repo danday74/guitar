786a28721970045faac07e7f8124d245fef7c912
Daniel Lewis <daniellewis777@gmail.com>
2024-01-11 01:08:46 +0000

    utils

--- src/app/utils/chord-utils.ts
+++ src/app/utils/chord-utils.ts
@@ -1,18 +1,15 @@
 import { onlyIncludes, replaceAt } from './string-utils'
 import { TChord } from '@components/chord-highlighter/types/t-chord'
-import { getChords } from '@components/chord-highlighter/data/chords'
-import { chordOptions } from '@components/chord-highlighter/interfaces/i-chord-options'
+import { chords } from '@components/chord-highlighter/data/chords'
 
 export const toStandard = (chord: TChord): TChord => {
   if (chord.length > 1) {
     if (chord[1] === '#') chord = replaceAt(chord, 1, '♯') as TChord
     if (chord[1].toLowerCase() === 'b') chord = replaceAt(chord, 1, '♭') as TChord
   }
-  const chordsStandard: TChord[] = getChords(chordOptions.standard)
-  return chordsStandard.find((chordStandard: TChord): boolean => chordStandard.toLowerCase() === chord.toLowerCase())
+  return chords.standard.find((chordStandard: TChord): boolean => chordStandard.toLowerCase() === chord.toLowerCase())
 }
 
 export const getIsChordLine = (line: string): boolean => {
-  const chords: TChord[] = getChords(chordOptions.lowerAll)
-  return onlyIncludes(line.toLowerCase(), chords)
+  return onlyIncludes(line.toLowerCase(), chords.lowerAll)
 }
