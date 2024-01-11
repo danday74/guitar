d6b587b49e60b66c1325cf708ea727336eebfbe9
Daniel Lewis <daniellewis777@gmail.com>
2024-01-11 00:41:40 +0000

    utils

--- src/app/utils/chord-utils.ts
+++ src/app/utils/chord-utils.ts
@@ -1,17 +1,18 @@
 import { onlyIncludes, replaceAt } from './string-utils'
 import { TChord } from '@components/chord-highlighter/types/t-chord'
 import { getChords } from '@components/chord-highlighter/data/chords'
-import { defaultChordOptions, IChordOptions } from '@components/chord-highlighter/interfaces/i-chord-options'
+import { chordOptions } from '@components/chord-highlighter/interfaces/i-chord-options'
 
 export const toStandard = (chord: TChord): TChord => {
   if (chord.length > 1) {
     if (chord[1] === '#') chord = replaceAt(chord, 1, '♯') as TChord
-    if (chord[1] === 'b') chord = replaceAt(chord, 1, '♭') as TChord
+    if (chord[1].toLowerCase() === 'b') chord = replaceAt(chord, 1, '♭') as TChord
   }
-  return chord
+  const chordsStandard: TChord[] = getChords(chordOptions.standard)
+  return chordsStandard.find((chordStandard: TChord): boolean => chordStandard.toLowerCase() === chord.toLowerCase())
 }
 
-export const getIsChordLine = (line: string, options: IChordOptions = defaultChordOptions): boolean => {
-  const chords: TChord[] = getChords(options)
-  return onlyIncludes(line, chords)
+export const getIsChordLine = (line: string): boolean => {
+  const chords: TChord[] = getChords(chordOptions.lowerAll)
+  return onlyIncludes(line.toLowerCase(), chords)
 }
