88a0c8435e5c3b5fddc7d6b6d97acf564985c1d9
Daniel Lewis <daniellewis777@gmail.com>
2024-01-18 08:00:26 +0000

    utils

--- src/app/utils/chord-utils.ts
+++ src/app/utils/chord-utils.ts
@@ -1,6 +1,7 @@
 import { onlyIncludes, replaceAt } from './string-utils'
 import { TChord } from '@components/chord-highlighter/types/t-chord'
 import { chords } from '@components/chord-highlighter/data/chords'
+import { TLineType } from '@ttypes/t-line-type'
 
 export const toStandard = (chord: TChord): TChord => {
   if (chord.length > 1) {
@@ -12,4 +13,10 @@ export const toStandard = (chord: TChord): TChord => {
 
 export const getIsChordLine = (line: string): boolean => {
   return onlyIncludes(line.toLowerCase(), chords.lowerAll)
+}
+
+export const getLineType = (line: string): TLineType => {
+  if (getIsChordLine(line)) return 'chord'
+  if (line.trim() === '') return 'blank'
+  return 'lyric'
 }
