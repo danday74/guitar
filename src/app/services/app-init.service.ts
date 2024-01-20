d22babfe0d9f5f5388c35ec4ded34e25b09de8f7
Daniel Lewis <daniellewis777@gmail.com>
2024-01-20 22:41:07 +0000

    app init

--- src/app/services/app-init.service.ts
+++ src/app/services/app-init.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core'
+import { dicts } from '@utils/dicts'
+import Typo from 'typo-js'
 
 @Injectable({ providedIn: 'root' })
 export class AppInitService {
   async init(): Promise<boolean> {
+    return this.initDicts()
+  }
+
+  async initDicts(): Promise<boolean> {
+
+    dicts.forEach((dict: Typo) => {
+      // @ts-ignore
+      if (dict.loaded === false) throw Error(`failure to load dict ${dict.dictionary}`)
+    })
+
     return true
   }
 }
