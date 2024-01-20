932eb9947f221d7075c298b9c96279f270685b12
Daniel Lewis <daniellewis777@gmail.com>
2024-01-20 22:28:01 +0000

    main

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import { appConfig } from './app/app.config'
 import { AppComponent } from './app/app.component'
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err))
+  .catch((err) => {
+    // TODO: logger util
+    console.error('bootstrapApp error', err.message)
+  })
