c5fc7e7feba5660301b36f6342ca0e949d8fc516
Daniel Lewis <daniellewis777@gmail.com>
2024-02-09 01:01:35 +0000

    my-linter

--- src/app/components/chord-highlighter/data/my-linter.ts
+++ src/app/components/chord-highlighter/data/my-linter.ts
@@ -1,10 +1,12 @@
 import { Diagnostic, linter } from '@codemirror/lint'
-import { Line, Text } from '@codemirror/state'
+import { ChangeSpec, Line, Text } from '@codemirror/state'
 import { TLineType } from '@ttypes/t-line-type'
-import { getLineType } from '@utils/chord-utils'
+import { getChordCount, getIsChordLine, getLineType } from '@utils/chord-utils'
 import { get } from 'lodash-es'
 import { EditorView } from '@codemirror/view'
 import { ILineType } from '@components/chord-highlighter/interfaces/i-line-type'
+import { ISpellCheckLine } from '@interfaces/i-spell-check'
+import { spellCheckLine } from '@utils/lang-utils'
 
 // https://codemirror.net/examples/lint
 // https://codemirror.net/docs/ref/#lint.Diagnostic
@@ -25,7 +27,7 @@ const createDiagnostic = (myLine: ILineType, message: string, markClass: string)
 
 export const myLinter = () => {
   return linter((view: EditorView) => {
-    const diagnostics: Diagnostic[] = []
+    let diagnostics: Diagnostic[] = []
 
     const doc: Text = view.state.doc
     const myLines: ILineType[] = []
@@ -57,6 +59,79 @@ export const myLinter = () => {
           const diagnostic: Diagnostic = createDiagnostic(myLine, 'lyrics - line before must be chords', 'lyrics-line-before')
           diagnostics.push(diagnostic)
         }
+      }
+
+      if (myLine.lineType === 'lyrics') {
+
+        const spellings: ISpellCheckLine[] = spellCheckLine(myLine.line.text)
+
+        const typos: Diagnostic[] = spellings.map((scl: ISpellCheckLine) => {
+
+          return {
+            from: myLine.line.from + scl.idx,
+            to: myLine.line.from + scl.idx + scl.word.length,
+            severity: 'hint',
+            message: `suggestions`,
+            markClass: 'typo',
+            actions: scl.suggestions.map((suggestion: string) => {
+              return {
+                name: suggestion,
+
+                // TODO: Unit test this
+                // e.g.
+                //   E Db
+                // herro there
+
+                apply(view: EditorView, from: number, to: number) {
+
+                  let changes: ChangeSpec[] = [{ from, to, insert: suggestion }]
+
+                  if (prevLine && prevLine.lineType === 'chords') {
+                    const diff: number = suggestion.length - scl.word.length
+
+                    if (diff !== 0) {
+
+                      const start: string = prevLine.line.text.substring(0, scl.idx)
+                      let middle: string
+                      const end: string = prevLine.line.text.substring(scl.idx + scl.word.length)
+
+                      if (diff > 0) { // add chars
+                        middle = prevLine.line.text.substring(scl.idx, scl.idx + scl.word.length) + ' '.repeat(diff)
+                      }
+
+                      if (diff < 0) { // remove chars
+                        middle = prevLine.line.text.substring(scl.idx, scl.idx + suggestion.length)
+                      }
+
+                      const newPrevLineText: string = start + middle + end
+
+                      const isChordLine: boolean = getIsChordLine(newPrevLineText)
+                      // TODO: checking chord count is not good enough - need to check actual chords are identical and in correct order
+                      const chordCount: number = getChordCount(prevLine.line.text)
+                      const newChordCount: number = getChordCount(newPrevLineText)
+
+                      if (isChordLine && chordCount === newChordCount) {
+                        const change: ChangeSpec = [{
+                          from: prevLine.line.from,
+                          to: prevLine.line.to,
+                          insert: newPrevLineText
+                        }]
+                        changes = [...changes, change]
+                      } else {
+                        // TODO
+                        console.warn('Please make manual changes to chords line to allow for typo fix')
+                      }
+                    }
+                  }
+
+                  return view.dispatch({ changes })
+                }
+              }
+            })
+          } as Diagnostic
+        })
+
+        diagnostics = [...diagnostics, ...typos]
       }
     })
 
